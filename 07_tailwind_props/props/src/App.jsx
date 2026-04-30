306be8b80b0bd641f1c817f5b69cda1ac2674631
zeshanabs <zeeshanabbasrao@gmail.com>
2026-04-30 15:11:14 +0500

    here we learn tailwindcs with props

---
+++ 07_tailwind_props/props/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from './components/Card'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-3xl font-bold underline'>hallow baii</h1>
+    <Card name="Hamza" Designamtion = "CEO"/>
+    <Card name="zeeshan" Designamtion = "ya b CEO"/>
+    <Card name="Awais" Designamtion = "Goli krwa raha"/>
+    </>
+  )
+}
+
+export default App
